7f84c98f18f21e8b2a28bef46bc22756a9299b07
Kristina Kruglova <141824826+kristina523@users.noreply.github.com>
2024-01-16 22:31:18 +0300

    Add files via upload

---
+++ C++/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+std::cout <<"Лишь тот,кто ждет-оценит встречу\n\n";
+std::cout <<"В разлуке нет ничьей вины-\n\n";
+std::cout <<"Кто не любил-тот гасит свечи\n\n";
+std::cout <<"Кто любил-тот горит внутри...\n\n";
+    }
